d7db78df0421081495f78c68b5ed70e96e680e76
Franco David <david_franco@outlook.com.ar>
2022-09-15 21:42:15 -0300

    proyecti terminado

--- AppFami/models.py
+++ AppFami/models.py
@@ -1,20 +1,17 @@
-from operator import truediv
+
 from django.db import models
-from datetime import datetime
+
 
 # Create your models here.
-from mailbox import NoSuchMailboxError
-from pyexpat import model
 
 
 
 # Create your models here.
-class familiar_1(models.Model):
+class Familiar(models.Model):
      nombre= models.CharField(max_length=50)
      apellido = models.CharField(max_length=50)
      edad = models.IntegerField()
-     fecha = datetime.today() # anio, mes, dia
-     feca_nacimiento = models.DateField()
+     fecha_nacimiento = models.DateField()
      
      
      
--- AppFami/views.py
+++ AppFami/views.py
@@ -1,36 +1,28 @@
 from re import template
 from django.shortcuts import render
-from .models import familiar_1
+from .models import Familiar
 from django.http import HttpResponse
-from django.template import Context, Template
+from django.template import Context, Template,loader
 
 # Create your views here.
 
 def fami (request):
     
-    gisel = familiar_1(nombre = "GISEL", apellido = "GOMEZ", edad = 41)
-    damian = familiar_1(nombre = "DAMIAN", apellido = "GOMEZ", edad = 37)
-    ricardo = familiar_1(nombre = "RICARDO", apellido = "GOMEZ", edad = 70)
+    gisel = Familiar(nombre = "GISEL", apellido = "GOMEZ", edad = 41, fecha_nacimiento= "1990-07-20")
+    damian = Familiar(nombre = "DAMIAN", apellido = "GOMEZ", edad = 37, fecha_nacimiento= "1984-04-29")
+    ricardo = Familiar(nombre = "RICARDO", apellido = "GOMEZ", edad = 70,  fecha_nacimiento= "1960-05-24")
     
     gisel.save()
     damian.save()
     ricardo.save()
     
-    texto = f"Mi familiar se llama {gisel.nombre} su apellido es {gisel.apellido} y tiene {gisel.edad} anios"
-    texto_2 = f"Mi familiar se llama {damian.nombre} su apellido es {damian.apellido} y tiene {damian.edad} anios"
+    familiares = [gisel,damian,ricardo]
     
-
-    diccionario = {"nombre": gisel.nombre,"apellido":gisel.apellido,"edad":gisel.edad,"fecha":gisel.fecha,
-                   "nombre_damian": damian.nombre,"apellido_damian":damian.apellido,"edad_damian":damian.edad,
-                   "nombre_ricardo": ricardo.nombre,"apellido_ricardo":ricardo.apellido,"edad_ricardo":ricardo.edad
-                   }
-   
-    
-    
-    mihtml = open("C:/Users/david/OneDrive/Escritorio/PrimerMTV/ProyectoFami/templates/templade1.html")
-    plantilla = Template(mihtml.read())
-    mihtml.close()
-    
-    contexto = Context(diccionario)
+    plantilla = loader.get_template("templade1.html")
+    contexto = {"familiares":familiares}
     documento = plantilla.render(contexto)
     return HttpResponse(documento)
+    
+    
+    
+    
